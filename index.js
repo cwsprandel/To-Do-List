487396d421ffc9503f642a498833739ddb051b0c
cwsprandel <110702891+cwsprandel@users.noreply.github.com>
2023-05-01 15:33:16 -0500

    Creating Checkbox Logic for Completing Tasks

--- index.js
+++ index.js
@@ -7,8 +7,10 @@
     //render
     //
 
-const todoObjectOne = {title: "Title One", description: "Description One", date: "4/29/2023"}
-const todoObjectTwo = {title: "Title Two", description: "Description Two", date: "4/27/2023"}
+
+    //add a project property
+const todoObjectOne = {title: "Title One", description: "Description One", date: "4/29/2023", completed: true}
+const todoObjectTwo = {title: "Title Two", description: "Description Two", date: "4/27/2023", completed: false}
 const todoArray = [todoObjectOne, todoObjectTwo];
 localStorage.setItem("todos", JSON.stringify(todoArray));
 
@@ -36,9 +38,17 @@ const todoLogic = (() => {
         todoScreen.render();
     }
 
+    const updateToDoStatus = (todoIndexPosition) => {
+        updatedToDoArray[todoIndexPosition].completed = !updatedToDoArray[todoIndexPosition].completed
+        localStorage.setItem("todos", JSON.stringify(updatedToDoArray));
+
+        todoScreen.render();
+    }
+
     return {
         addNewToDoItem,
         deleteSelectedToDoItem,
+        updateToDoStatus,
         updatedToDoArray
     }
 })();
@@ -60,6 +70,9 @@ todoScreen = (() => {
         //creating the to do checkbox
         const todoCheckbox = document.createElement("button");
         todoCheckbox.setAttribute("class", "todoCheckbox");
+        todoCheckbox.onclick = function () {
+            todoLogic.updateToDoStatus(indexPosition);
+        };
         //creating the to do Title and setting it to the object title
         const todoTitle = document.createElement("div");
         todoTitle.setAttribute("class", "todoTitle");
@@ -89,14 +102,19 @@ todoScreen = (() => {
             todoLogic.deleteSelectedToDoItem(indexPosition);
         };
         //creating and setting the checkbox SVG
-        const emptyCheckboxSVG = document.createElementNS("http://www.w3.org/2000/svg","svg");
-        emptyCheckboxSVG.setAttribute("height", "24px");
-        emptyCheckboxSVG.setAttribute("fill", "currentColor");
-        emptyCheckboxSVG.setAttribute("viewBox", "0 0 16 16");
-        const emptyCheckboxPath = document.createElementNS("http://www.w3.org/2000/svg","path");
-        emptyCheckboxPath.setAttribute("d", "M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z");
-        emptyCheckboxSVG.appendChild(emptyCheckboxPath);
-        todoCheckbox.appendChild(emptyCheckboxSVG);
+        const checkboxSVG = document.createElementNS("http://www.w3.org/2000/svg","svg");
+        checkboxSVG.setAttribute("height", "24px");
+        checkboxSVG.setAttribute("fill", "currentColor");
+        checkboxSVG.setAttribute("viewBox", "0 0 16 16");
+        const checkboxPath = document.createElementNS("http://www.w3.org/2000/svg","path");
+        checkboxPath.setAttribute("d", "M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z");
+        checkboxSVG.appendChild(checkboxPath);
+        if (todoObject.completed) {
+            const checkboxCheckedPath = document.createElementNS("http://www.w3.org/2000/svg","path");
+            checkboxCheckedPath.setAttribute("d","M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z");
+            checkboxSVG.appendChild(checkboxCheckedPath);
+        } 
+        todoCheckbox.appendChild(checkboxSVG);
         //creating and setting the edit SVG
         const editSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         editSVG.setAttribute("height", "24px");
@@ -144,7 +162,7 @@ const newToDoButton = document.getElementById("addNewToDoItemButton")
 newToDoButton.addEventListener("click", () => {
     //newToDoItem();
     //console.log(localStorage.testKey);
-    const todoObjectThree = {title: "Title Three", description: "Description Three", date: "4/30/2023"};
+    const todoObjectThree = {title: "Title Three", description: "Description Three", date: "4/30/2023", completed: true};
     todoLogic.addNewToDoItem(todoObjectThree);
     //const todoForm = document.getElementById("addNewToDoItemForm");
     //todoForm.style.display = "block";
