6836e2231de9a70bb5b1b3f8f6932baad035fc95
cwsprandel <110702891+cwsprandel@users.noreply.github.com>
2023-05-01 09:00:11 -0500

    Adding some comments

--- index.js
+++ index.js
@@ -49,35 +49,39 @@ todoScreen = (() => {
     }
 
     const newToDoItem = (todoObject) => {
-
+        //creating the to do task container
         const todoItem = document.createElement("div");
         todoItem.setAttribute("class", "todoItems");
-    
+        //creating the to do checkbox
         const todoCheckbox = document.createElement("button");
         todoCheckbox.setAttribute("class", "todoCheckbox");
-    
+        //creating the to do Title and setting it to the object title
         const todoTitle = document.createElement("div");
         todoTitle.setAttribute("class", "todoTitle");
         todoTitle.textContent = todoObject.title;
-    
+        //creating the to do Description and setting it to the object description
         const todoDescription = document.createElement("div");
         todoDescription.setAttribute("class", "todoDescription");
         todoDescription.textContent = todoObject.description;
-    
+        //creating the to do Due Date and setting it to the object date
         const todoDueDate = document.createElement("div");
         todoDueDate.setAttribute("class", "todoDueDate");
         todoDueDate.textContent = todoObject.date;
-    
+        //creating the to do Priority 
+    //need to get object priority 
+    //need to set to object priority
         const todoPriority = document.createElement("div");
         todoPriority.setAttribute("class", "todoPriority");
         todoPriority.textContent = "priority";
-    
+        //creating the to do Edit container
+    //change div to button, same as checkbox
         const todoEdit = document.createElement("div");
         todoEdit.setAttribute("class", "todoEdit");
-    
+        //creating the to do Delete container
+    //change div to button, same as checkbox
         const todoDelete = document.createElement("div");
         todoDelete.setAttribute("class", "todoDelete");
-    
+        //creating and setting the checkbox SVG
         const emptyCheckboxSVG = document.createElementNS("http://www.w3.org/2000/svg","svg");
         emptyCheckboxSVG.setAttribute("height", "24px");
         emptyCheckboxSVG.setAttribute("fill", "currentColor");
@@ -86,7 +90,7 @@ todoScreen = (() => {
         emptyCheckboxPath.setAttribute("d", "M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z");
         emptyCheckboxSVG.appendChild(emptyCheckboxPath);
         todoCheckbox.appendChild(emptyCheckboxSVG);
-    
+        //creating and setting the edit SVG
         const editSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         editSVG.setAttribute("height", "24px");
         editSVG.setAttribute("fill", "currentColor");
@@ -98,7 +102,7 @@ todoScreen = (() => {
         editSVG.appendChild(editPathOne);
         editSVG.appendChild(editPathTwo);
         todoEdit.appendChild(editSVG);
-    
+        //creating and setting the delete SVG
         const deleteSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         deleteSVG.setAttribute("height", "24px");
         deleteSVG.setAttribute("fill", "currentColor");
@@ -107,7 +111,7 @@ todoScreen = (() => {
         deletePath.setAttribute("d", "M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z");
         deleteSVG.appendChild(deletePath);
         todoDelete.appendChild(deleteSVG);
-    
+        //adding all elements to the to do task container
         todoItem.appendChild(todoCheckbox);
         todoItem.appendChild(todoTitle);
         todoItem.appendChild(todoDescription);
@@ -115,6 +119,7 @@ todoScreen = (() => {
         todoItem.appendChild(todoPriority);
         todoItem.appendChild(todoEdit);
         todoItem.appendChild(todoDelete);
+        //adding to do task container to the items container
         todoItemsContainer.appendChild(todoItem);
     }
 
@@ -122,11 +127,6 @@ todoScreen = (() => {
         render
     }
 })();
-
-
-
-
-
 
 
 const selectCheckbox = () => {
